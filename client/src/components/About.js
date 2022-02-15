46d14289d3b10d283393534741335cc8172539a1
Tim <timothy.george.taylor@gmail.com>
2022-02-15 11:17:19 -0700

    updated about

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -22,7 +22,11 @@ I'm a software developer using mostly Javascript and Ruby. I appreciate good UI,
         <p class="about-content">
 Using my experience as a luthier and instrument designer I treat my process of building software as building an instrument: something that is both simple & easily discoverable yet rewards deeper interaction & study. In my free time I enjoy coding musical controllers/interfaces for both the browser and various hardware devices.
         </p>
+        <p class="about-content">
+My other interests include: synthesis/sound/instrument design, iterative processes, generative patterns, Andrei Tarkovsky films, 80-90s alt/indie rock, Boston Celtics basketball, chess, Camusian restraint-as-rebellion minimalism, creative coding, balancing free improvisation with intentionality, Chris Farley comedies, Bauhausian modernism, the electric guitar + delay pedals, low-impact kettlebell workouts, skateboarding, techno, and running.
+        </p>
         <br />
+        <p>contact @ tim [at] bvwtgt [dot] xyz</p>
         <a href="https://github.com/timothy-taylor">Github</a>
         <a href="https://www.linkedin.com/in/timothy-g-taylor">LinkedIn</a>
         <br />
