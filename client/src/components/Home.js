c87fdd821d1486b1fe8cc7204d7b8e5f32f8312a
Tim <timothy.george.taylor@gmail.com>
2022-03-10 12:36:22 -0700

    style(client) updated Home.js text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,10 +5,9 @@ const Home = () => (
     <main>
       <h2>Hi and welcome.</h2>
       <p className="about-content">
-        This place houses the writings and processes of Tim Taylor as he
-        contemplates technology, design, process, and the connection of those
-        things to the human experience. Always a beginner; forever a work in
-        progress.
+        This place houses the writings and projects of Tim Taylor as he
+        explores technology, design, process, and sound. Always a beginner;
+        forever a work in progress.
       </p>
     </main>
   </Layout>
